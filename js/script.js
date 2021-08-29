e884714537ec957a131f3e1b17c5e7e9c2277d76
DamianoLandi <damianolandi01@gmail.com>
2021-08-29 17:07:23 +0200

    Cambio Chat al ckick

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ const root = new Vue({
         data,
     },
     methods:{
+        switchChat(i){
+            for(key in this.data.contacts){
+                this.data.contacts[key].visible = false;
+            }
+            this.data.contacts[i].visible = true;
+        }
+    },
 
-    }
+    created: function () {
+        for(key in this.data.contacts){
+            this.data.contacts[key].visible = false;
+        }
+        this.data.contacts[0].visible = true;
+      }
 });
